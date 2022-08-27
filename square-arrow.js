9e40d3e6849d40b2c8e7eb02413568ebc8014e8a
Shonkor Das <94380595+shonkor-das@users.noreply.github.com>
2022-08-27 14:56:47 +0600

    average sum elements added

--- square-arrow.js
+++ square-arrow.js
@@ -4,7 +4,8 @@ const square = (element) => element ** 2
 const a = [1, 2, 3, 4, 5];
 const a2 = a.map(square);
 console.log(a2);
-  
+
+// sum square element 
 var sum = 0;
 
 for (let i = 0; i < a2.length; i++) {
@@ -12,4 +13,9 @@ for (let i = 0; i < a2.length; i++) {
 }
 console.log(sum);
 
+// average of the sum of the squared elements 
+
+const avrg = 0;
+const average = sum / a2.length;
+console.log(average);
 
